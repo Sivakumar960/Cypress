0e8425f4d82aed470fe2ae2fa21f265c3a50a65f
Sivakumar Dacharla <sivakumar.dacharla@gmail.com>
2022-03-22 14:33:08 +0530

    Changed

--- cypress/integration/accessi.spec.js
+++ cypress/integration/accessi.spec.js
@@ -1,10 +1,23 @@
 import axe, { AxeResults } from 'axe-core'
 import {createHtmlReport} from 'axe-html-reporter'
 
+
 describe('Sample Test Suite',()=>{
     it('To test a sample application using Cypress IO',()=>{
         cy.visit("https://www.amazon.in/")
         cy.injectAxe()
-        cy.checkA11y(null,null,report) 
-    })
+        
+        cy.checkA11y(null,{
+            includedImpacts: ['serious'],
+            runOnly: {
+                type: 'tag',
+                values: ['wcag2a']
+              }},terminallog,true)
+        function terminallog(violations) {
+           cy.writeFile("./build/report.json",violations)
+        }
+
+        
+       
+   })
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -28,18 +28,4 @@ module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
 
 }
-module.exports = (on, config) => {
-  on('task', {
-    log(message) {
-      console.log(message)
-
-      return null
-    },
-    table(message) {
-      console.table(message)
-
-      return null
-    }
-  })
-}
 
